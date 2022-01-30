0de4f44773f9aead38e0b095075492b0e553498a
Yashas BN <68140716+yashas1145@users.noreply.github.com>
2022-01-30 21:34:23 +0530

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,19 +1,15 @@
-from sklearn.model_selection import train_test_split
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split as tts
+from sklearn.neighbors import KNeighborsClassifier as knc
+from sklearn.metrics import classification_report as cr, confusion_matrix as cm
+from sklearn import datasets as dt
 
-from sklearn import datasets
-iris=datasets.load_iris()
-iris_data=iris.data
-iris_labels=iris.target
-print(iris_data)
-print(iris_labels)
-x_train, x_test, y_train, y_test=train_test_split(iris_data,iris_labels,test_size=0.30)
+iris = dt.load_iris()
+data, label = iris.data, iris.target
 
-classifier=KNeighborsClassifier(n_neighbors=5)
-classifier.fit(x_train,y_train)
-y_pred=classifier.predict(x_test)
-print('confusion matrix is as follows')
-print(confusion_matrix(y_test,y_pred))
-print('Accuracy metrics')
-print(classification_report(y_test,y_pred))
+xTr, xT, yTr, yT = tts(data, label, test_size=0.30)
+kmc = knc(5)
+kmc.fit(xTr, yTr)
+yPr = kmc.predict(xT)
+
+print(cm(yT, yPr))
+print(cr(yT, yPr))
