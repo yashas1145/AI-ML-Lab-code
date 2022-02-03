7d5edbea745d24aa673b692c4207c5d44650f840
Yashas BN <68140716+yashas1145@users.noreply.github.com>
2022-02-03 09:15:20 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,23 +1,43 @@
-import numpy as np
-import matplotlib.pyplot as plt
+from re import X
+import matplotlib.pyplot as plt, pandas as pd, numpy as np
 
-def local_regression(x0, X, Y, tau):
-    x0 = [1, x0]   
-    X = [[1, i] for i in X]
-    X = np.asarray(X)
-    xw = (X.T) * np.exp(np.sum((X - x0) ** 2, axis=1) / (-2 * tau))
-    beta = np.linalg.pinv(xw @ X) @ xw @ Y @ x0  
-    return beta    
+def kernel(p, x, k):
+    m, n = np.shape(x)
+    w = np.mat(np.eye((m)))
 
-def draw(tau):
-    prediction = [local_regression(x0, X, Y, tau) for x0 in domain]
-    plt.plot(X, Y, 'o', color='black')
-    plt.plot(domain, prediction, color='red')
+    for j in range(m):
+        d = p - X[j]
+        w[j,j] = np.exp(d * d.T/(-2.0*k**2))
+    return w
+
+def localWeight(p, x, y, k):
+    w = kernel(p, x, k)
+    W = (X.T*(w*X)).I * (X.T*(w*y.T))
+    return W
+
+def lwr(x, y, k):
+    m, n = np.shape(x)
+    yPr = np.zeros(m)
+    for i in range(m):
+        yPr[i] = x[i] * localWeight(x[i], x, y, k)
+    return yPr
+
+def graphPlot(x, yPr):
+    sortIndex = X[:,1].argsort(0)
+    xSort = X[sortIndex][:,0]
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.scatter(bill, tip, color="green")
+    ax.plot(xSort[:,1], yPr[sortIndex], color="red", linewidth=3)
+    plt.xlabel("Total bill")
+    plt.ylabel("Tip")
     plt.show()
 
-X = np.linspace(-3, 3, num=1000)
-domain = X
-Y = np.log(np.abs(X ** 2 - 1) + .5)
-
-draw(10)
-draw(0.001)
+data = pd.read_csv("regression.csv")
+bill, tip = np.array(data.total_bill), np.array(data.tip)
+mBill, mTip = np.mat(bill), np.mat(tip)
+m = np.shape(mBill)[1]
+one = np.mat(np.ones(m))
+X = np.hstack((one.T, mBill.T))
+yPr = lwr(X, mTip, 3)
+graphPlot(X, yPr)
