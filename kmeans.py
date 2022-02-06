dcfaec96ce7c4eb05bc12254f1b6e65aa2a8dc7f
Yashas BN <68140716+yashas1145@users.noreply.github.com>
2022-02-06 13:00:45 +0530

    Update kmeans.py

--- kmeans.py
+++ kmeans.py
@@ -1,95 +1,35 @@
-import csv, random, math
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
+from pyparsing import col
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn.mixture import GaussianMixture
 
-def loadcsv(file):
-    line = csv.reader(open(file, "r"))
-    data = list(line)
-    for i in range(len(data)):
-        data[i] = [float(x) for x in data[i]]
-    return data
+data = load_iris()
+X = pd.DataFrame(data.data, columns=['Sepal_Length', 'Sepal_Width', 'Petal_Length', 'Petal_Width'])
+y = pd.DataFrame(data.target, columns=['Target'])
 
-def splitData(data, splitRatio):
-    trainSize, trainSet = int(len(data) * splitRatio), []
-    copy = list(data)
-    
-    while len(trainSet) < trainSize:
-        index = random.randrange(len(copy))
-        trainSet.append(copy.pop(index))
-    return [trainSet, copy]
+plt.figure(figsize=(21, 7))
+colormap = np.array(['Red', 'Green', 'Black'])
 
-def splitClass(data):
-    seperate = {}
-    for i in range(len(data)):
-        vector = data[i]
-        if vector[-1] not in seperate:
-            seperate[vector[-1]] = []
-        seperate[vector[-1]].append(vector)
-    return seperate
+plt.subplot(1,3,1)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[y.Target], s=40)
 
-def mean(number):
-    return sum(number)/float(len(number))
+model = KMeans(3)
+model.fit(X)
+yPred = np.choose(model.labels_, [0,1,2]).astype(np.int64)
 
-def stdev(number):
-    avg = mean(number)
-    variance = sum([pow(x-avg, 2) for x in number])/float(len(number)-1)
-    return math.sqrt(variance)
+plt.subplot(1,3,2)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[yPred], s=40)
 
-def summarize(data):
-    summary = [(mean(attr), stdev(attr)) for attr in zip(*data)]
-    del summary[-1]
-    return summary
+scaler = preprocessing.StandardScaler()
+xsa = scaler.fit_transform(X)
+xs = pd.DataFrame(xsa, columns=X.columns)
+gmm = GaussianMixture(3)
+gmm.fit(xs)
+yCluster = gmm.predict(xs)
 
-def summarizeByClass(data):
-    seperate = splitClass(data)
-    summary = {}
-    for classValue, instance in seperate.items():
-        summary[classValue] = summarize(instance)
-    return summary
+plt.subplot(1,3,3)
+plt.scatter(X.Petal_Length, X.Petal_Width, c=colormap[yCluster], s=40)
 
-def calculateProbability(x, mean, stdev):
-    exp = math.exp(-(math.pow(x-mean, 2)/(2*math.pow(stdev,2))))
-    return (1/(math.sqrt(2*math.pi) * stdev)) * exp
-
-def calculateClassProbability(summary, vector):
-    prob = {}
-    for classValue, classSummary in summary.items():
-        prob[classValue] = 1
-        for i in range(len(classSummary)):
-            mean, stdev = classSummary[i]
-            x = vector[i]
-            prob[classValue] *= calculateProbability(x, mean, stdev)
-        return prob
-
-def predict(summary, vector):
-    prob = calculateClassProbability(summary, vector)
-    bestLabel, bestProb = None, -1
-    for classValue, prob in prob.items():
-        if bestLabel is None or prob > bestProb:
-            bestProb = prob
-            bestLabel = classValue
-    return bestLabel
-
-def getPrediction(summary, test):
-    pred = []
-    for i in range(len(test)):
-        result = predict(summary, test[i])
-        pred.append(result)
-    return pred
-
-def getAccuracy(test, pred):
-    correct = 0
-    for i in range(len(test)):
-        if test[i][-1] == pred[i]:
-            correct += 1
-    return (correct/float(len(test))) * 100.0
-
-def main():
-    data = loadcsv("nbc.csv")
-    trainSet, testSet = splitData(data, 0.67)
-    print("Split {} rows into train {} and test {} rows".format(len(data), len(trainSet), len(testSet)))
-    summary = summarizeByClass(trainSet)
-    pred = getPrediction(summary, testSet)
-    accuracy = getAccuracy(testSet, pred)
-    print("Accuracy of the classifier is: {}".format(accuracy))
-
-if __name__ == "__main__":
-    main()
+plt.show()
