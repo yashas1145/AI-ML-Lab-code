f623976f7a53415631e623a926425e9cf2303b1f
Yashas BN <68140716+yashas1145@users.noreply.github.com>
2022-03-29 20:21:36 +0530

    Update regression.py

--- regression.py
+++ regression.py
@@ -1,18 +1,16 @@
-from re import X
-import matplotlib.pyplot as plt, pandas as pd, numpy as np
+import pandas as pd, numpy as np, matplotlib.pyplot as plt
 
 def kernel(p, x, k):
     m, n = np.shape(x)
     w = np.mat(np.eye((m)))
-
-    for j in range(m):
-        d = p - X[j]
-        w[j,j] = np.exp(d * d.T/(-2.0*k**2))
+    for i in range(m):
+        d = p - X[i]
+        w[i,i] = np.exp(-(d*d.T)/(2.0*k**2))
     return w
 
 def localWeight(p, x, y, k):
     w = kernel(p, x, k)
-    W = (X.T*(w*X)).I * (X.T*(w*y.T))
+    W = (X.T * (w*X)).I * (X.T * (w*y.T))
     return W
 
 def lwr(x, y, k):
@@ -22,22 +20,17 @@ def lwr(x, y, k):
         yPr[i] = x[i] * localWeight(x[i], x, y, k)
     return yPr
 
-def graphPlot(x, yPr):
-    sortIndex = X[:,1].argsort(0)
-    xSort = X[sortIndex][:,0]
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    ax.scatter(bill, tip, color="green")
-    ax.plot(xSort[:,1], yPr[sortIndex], color="red", linewidth=3)
-    plt.xlabel("Total bill")
-    plt.ylabel("Tip")
+def graph(x, y):
+    si = X[:,1].argsort(0)
+    xs = X[si][:,0]
+    plt.subplot(1,1,1)
+    plt.scatter(np.array(data.bill), np.array(data.tip))
+    plt.plot(xs[:,1], Y[si], color='red')
     plt.show()
 
-data = pd.read_csv("regression.csv")
-bill, tip = np.array(data.total_bill), np.array(data.tip)
-mBill, mTip = np.mat(bill), np.mat(tip)
-m = np.shape(mBill)[1]
-one = np.mat(np.ones(m))
+data = pd.read_csv('regression.csv')
+mBill, mTip = np.mat(np.array(data.bill)), np.mat(np.array(data.tip))
+one = np.mat(np.ones(np.shape(mBill)[1]))
 X = np.hstack((one.T, mBill.T))
-yPr = lwr(X, mTip, 3)
-graphPlot(X, yPr)
+Y = lwr(X, mTip, 3)
+graph(X, Y)
